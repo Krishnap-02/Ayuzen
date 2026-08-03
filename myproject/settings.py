84bf6bad438a61cda38268828cd9612746e6b37f
Krishnap-02 <krishp110802@gmail.com>
2026-08-03 11:14:55 +0530

    Use environment variables for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k0ho8=-*=sp!s=tzn73n@u9a+i-@e%ek(wv_g@x0i41r$)wo2-'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-k0ho8=-*=sp!s=tzn73n@u9a+i-@e%ek(wv_g@x0i41r$)wo2-"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
